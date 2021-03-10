7f2eefc4415a5f46d37a5c7a4b597e6a7ee5834f
you.wu <1394317135@163.com>
2021-03-10 11:48:52 +0800

    common updating

---
+++ Hdony-LC/SolutionInOrder/LeetCode-1785-Minimum-Elements-to-Add-to-Form-a-Given-Sum/LC-1785.h
@@ -0,0 +1,42 @@
+/*
+ * 给你一个整数数组 nums ，和两个整数 limit 与 goal 。
+ * 数组 nums 有一条重要属性：abs(nums[i]) <= limit 。
+ * 返回使数组元素总和等于 goal 所需要向数组中添加的 最少元素数量 ，添加元素 不应改变 数组中 abs(nums[i]) <= limit 这一属性。
+ * 注意，如果 x >= 0 ，那么 abs(x) 等于 x ；否则，等于 -x 。
+ */
+
+/*
+ * 示例 1：
+ * 输入：nums = [1,-1,1], limit = 3, goal = -4
+ * 输出：2
+ * 解释：可以将 -2 和 -3 添加到数组中，数组的元素总和变为 1 - 1 + 1 - 2 - 3 = -4 。
+ */
+
+/*
+ * 示例 2：
+ * 输入：nums = [1,-10,9,1], limit = 100, goal = 0
+ * 输出：1
+ */
+
+/*
+ * 提示：
+ * 1 <= nums.length <= 10^5
+ * 1 <= limit <= 10^6
+ * -limit <= nums[i] <= limit
+ * -10^9 <= goal <= 10^9
+ */
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minElements(vector<int>& nums, int limit, int goal) {
+        long sum = 0;
+        for (int num : nums)
+            sum += num;
+        return (abs(goal - sum) + limit - 1) / limit;
+    }
+};
